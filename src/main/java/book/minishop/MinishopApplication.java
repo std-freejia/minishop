50fb0f6a2c9969c0fa6832b5c05d123fa4ce1ef3
std-freejia <wtfreejia@gmail.com>
2022-01-10 21:59:18 +0900

    프로젝트 생성 및 view 환경설정

---
+++ src/main/java/book/minishop/MinishopApplication.java
@@ -0,0 +1,13 @@
+package book.minishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinishopApplication.class, args);
+	}
+
+}
