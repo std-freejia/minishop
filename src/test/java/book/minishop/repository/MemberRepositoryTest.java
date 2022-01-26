2f4d5a275f633a47e403543065c3c5fe3e56bb79
std-freejia <wtfreejia@gmail.com>
2022-01-26 21:14:34 +0900

    datasource, jpa, logging 설정하기

---
+++ src/test/java/book/minishop/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package book.minishop.repository;
+
+import book.minishop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // 테스트가 끝난 후 rollback 수행
+    @Rollback(false) // rollback 안함
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
